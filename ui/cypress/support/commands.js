8d8d607103ee2ace1231a83ff094f291a33e9858
Margarita <margaritaalcarazrojas@gmail.com>
2024-02-01 18:34:59 -0700

    fix style issues x2

--- ui/cypress/support/commands.js
+++ ui/cypress/support/commands.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 Cypress.Commands.add('createNewUser', ({ username, name, password }) => {
 	cy.request('POST', `${Cypress.env('BACKEND')}/users`, {
 		username,
